cbee4d5265776abebca78fedf42148967b5d207c
bamboechop <info@bamboechop.at>
2026-06-11 19:52:49 +0200

    feat(BMB-248): Toasty der Woche

--- src/common/constants/emotes.constant.ts
+++ src/common/constants/emotes.constant.ts
@@ -200,7 +200,7 @@ export const EMOTES: { lastModified: string; name: string; new: boolean; tier: '
     url: '/emotes/bamboe1Steer.gif',
   },
   {
-    lastModified: '2026-06-04',
+    lastModified: '2026-06-11',
     name: 'bamboe1ToastyDerWoche',
     new: true,
     tier: '1',
